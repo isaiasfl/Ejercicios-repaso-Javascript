4c998d6cc56d30fa1aa12fed98bce9561c384e72
ritaVD <ritavicdom@gmail.com>
2025-10-16 22:46:15 +0200

    update exercise-15

--- src/helpers/ejercicio-15/sistemasFavoritos.js
+++ src/helpers/ejercicio-15/sistemasFavoritos.js
@@ -1,7 +1,7 @@
 import { usuarios, productos } from './data.js';
 
-const favoritosUsuarios = [];
-const favoritosProductos = [];
+let favoritosUsuarios = [];
+let favoritosProductos = [];
 
 // Hacer un módulo, una funcion grande llamada sistemasFavoritos que tenga 
 // dentro el resto de funciones y en la ultima línea antes del cierre 
@@ -21,32 +21,126 @@ export const sistemasFavoritos = () => {
     if (tipo === 'producto'){
       const producto = productos
         .find(p => p.id === id);
+      const fav = {
+        tipo, 
+        id, 
+        fecha: new Date().toISOString()
+      };
       favoritosProductos
-        .push(producto);
+        .push(fav);
       localStorage.setItem('favoritosProductos', JSON.stringify(favoritosProductos));
-      console.log('Producto añadido: ', producto); 
+      console.log('Producto añadido: ', fav); 
     }else{
-                                                           const usuario = usuarios
-      .find(u => u.id === id);
-    favoritosUsuarios
-      .push(usuario);
-    localStorage.setItem('favoritosUsuarios', JSON.stringify(favoritosUsuarios)); 
-    console.log('Usuario añadido: ', usuario); 
+      const usuario = usuarios
+        .find(u => u.id === id);
+      const favU = {
+        tipo, 
+        id, 
+        fecha: new Date().toISOString()
+      };
+      favoritosUsuarios
+        .push(favU);
+      localStorage.setItem('favoritosUsuarios', JSON.stringify(favoritosUsuarios)); 
+      console.log('Usuario añadido: ', favU); 
     
 
-  }
+    }
+   
+  };
+  console.log(agregarFavorito('usuario', 1));
+  console.log(agregarFavorito('producto', 2));
+
+
+
+  const eliminarFavorito = (tipo = 'usuario', id= 1) => {
+    if (tipo === 'producto'){
+      const producto = productos
+        .find(p => p.id !== id);
+      const fav = {
+        tipo, 
+        id, 
+        fecha: new Date().toISOString()
+      };
+      localStorage.setItem('favoritosProductos', JSON.stringify(favoritosProductos));
+      console.log(`Producto con id ${id} eliminado.`);
+      
+    }
+
+  };
+  eliminarFavorito('producto', 2);
+
+
+  const esFavorito = (tipo = 'usuario', id = 1) => {
+    if (tipo === 'producto'){
+      const ids = favoritosProductos
+        .map(f => f.id);
+      return ids.includes(id);
+    }else{
+      const ids = favoritosUsuarios
+        .map(f => f.id);
+      return ids.includes(id);
+    }
+  }; 
+  console.log(esFavorito('producto', 2)); 
+  console.log(esFavorito('producto', 5));
+
+
+  const obtenerFavoritos= (tipo= 'usuario') => {
+    if (tipo === 'producto'){
+      return favoritosProductos;
+    }else{
+      return favoritosUsuarios;
+    }
+  };
+  console.log(obtenerFavoritos('usuario'));
+
+
+  const obtenerFavoritosConDetalles = (tipo = 'usuario') => {
+    if (tipo === 'producto'){
+      return favoritosProductos
+        .map(p => {
+          const producto = productos
+            .find(p => p.id === p.id);
+          return {
+            ...p,
+            ...producto,
+          };
+        });
+
+    }else{
+      return favoritosUsuarios.map(fav => {
+        const usuario = usuarios.find(u => u.id === fav.id);
+        return {
+          ...fav,
+          ...usuario
+        };
+      });
+    }  
+  };
+  console.log(obtenerFavoritosConDetalles('usuario'));
+
+  const filtrarFavsDate = (tipo = 'usuario', fecha = new Date().toISOString()) => {
+    if (tipo === 'producto'){
+      return favoritosProductos
+        .filter(f => f.fecha === fecha);
+    }else{
+      return favoritosUsuarios
+        .filter(f => f.fecha === fecha);
+
+    }
+  };
+  console.log(filtrarFavsDate('usuario', '2022-01-15'));
+
+  const buscarDentroFavorito = (tipo = 'usuario', texto = 'ana') => {
+    const favsDetallados = obtenerFavoritosConDetalles(tipo);
+    const palabra = texto.toLowerCase();
+    return favsDetallados.filter(fav => {
+      const datos = JSON.stringify(fav).toLowerCase();
+      return datos.includes(palabra);
+    });
+     
+  };
+  console.log(buscarDentroFavorito('usuario', 'ana'));
+
   
 };
-console.log(agregarFavorito('usuario', 1));
-console.log(agregarFavorito('producto', 2));
-
-
-
-const eliminarFavorito = (tipo, id) => {};
-const esFavorito = (tipo, id) => {}; 
-const obtenerFavoritos= (tipo) => {};
-const obtenerFavortiosConDetalles = (tipo) => {};
-
-
-};
- 
