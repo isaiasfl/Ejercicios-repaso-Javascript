c99fe2faf05b213385d75ec129f5fc9a6fad623b
sergioramirezmoron <sergiothd50@gmail.com>
2025-10-17 10:41:01 +0200

    delete app.js tester

--- src/app.js
+++ src/app.js
@@ -2,12 +2,9 @@
  * Lógica principal de la aplicación
  * Este archivo puede contener la inicialización y configuración global
  */
-import sistemaRecomendaciones from './helpers/ejercicio-21/sistemaRecomendaciones';
 // Función de inicialización
 export function initializeApp() {
   console.log('Inicializando aplicación...');
-  const { demostracionEjercicio21 } = sistemaRecomendaciones();
-  demostracionEjercicio21();
   // Aquí puedes configurar el estado inicial de tu aplicación
   // Por ejemplo: variables globales, configuraciones, etc.
 }
